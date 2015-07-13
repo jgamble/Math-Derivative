ed01d30896c2a16a3d7769c0c4c0fe7ea1fc6de2
John M. Gamble <jgamble@cpan.org>
2015-07-13 18:01:13 -0500

    POD and whitespace cleanup, use warnings, EXPORT and Carp, etc.

--- lib/Math/Derivative.pm
+++ lib/Math/Derivative.pm
@@ -16,11 +16,25 @@
 
 This Perl package exports functions for performing numerical first
 (B<Derivative1>) and second B<Derivative2>) order differentiation on
-vectors of data. They both take references to two arrays containing
-the x and y ordinates of the data and return an array of the 1st or
-2nd derivative at the given x ordinates. B<Derivative2> may optionally
-be given values to use for the first dervivative at the start and end
-points of the data - otherwiswe 'natural' values are used.
+vectors of data.
+
+=head2 FUNCTIONS
+
+The functions must be imported by name.
+
+=head3 Derivative1()
+
+Take the references to two arrays containing the x and y ordinates of
+the data, and return an array of the 1st derivative at the given x ordinates.
+
+=head3 Derivative2()
+
+Take references to two arrays containing the x and y ordinates of the
+data and return an array of the 2nd derivative at the given x ordinates.
+
+You may optionally give values to use as the first dervivatives at the
+start and end points of the data. If you don't, first derivative values
+will be calculated from the arrays for you.
 
 =head1 AUTHOR
 
@@ -29,63 +43,91 @@ John M. Gamble B<jgamble@cpan.org> (current maintainer)
 
 =cut
 
-require Exporter;
 package Math::Derivative;
-@ISA=qw(Exporter);
-@EXPORT_OK=qw(Derivative1 Derivative2);
+
+use 5.8.3;
+use Exporter;
+our(@ISA, @EXPORT_OK);
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(Derivative1 Derivative2);
+
+our $VERSION = 0.04;
+
 use strict;
+use warnings;
+use Carp;
 
-our $VERSION = 0.03;
-
-sub Derivative1 {
-    my ($x,$y)=@_;
+sub Derivative1
+{
+    my ($x, $y) = @_;
     my @y2;
-    my $n=$#{$x};
-    $y2[0]=($y->[1]-$y->[0])/($x->[1]-$x->[0]);
-    $y2[$n]=($y->[$n]-$y->[$n-1])/($x->[$n]-$x->[$n-1]);
-    my $i;
-    for($i=1; $i<$n; $i++) {
-	$y2[$i]=($y->[$i+1]-$y->[$i-1])/($x->[$i+1]-$x->[$i-1]);
+    my $n = $#{$x};
+
+    croak "X and Y array lengths don't match." unless ($n == $#{$y});
+
+    $y2[0] = ($y->[1] - $y->[0])/($x->[1] - $x->[0]);
+    $y2[$n] = ($y->[$n] - $y->[$n-1])/($x->[$n] - $x->[$n-1]);
+
+    for (my $i=1; $i<$n; $i++)
+    {
+	$y2[$i] = ($y->[$i+1] - $y->[$i-1])/($x->[$i+1] - $x->[$i-1]);
     }
+
     return @y2;
 }
 
-sub Derivative2 {
-    my ($x,$y,$yp1,$ypn)=@_;
-    my $n=$#{$x};
-    my ($i,@y2,@u);
-    if (!defined $yp1) {
-	$y2[0]=0; $u[0]=0;
+sub Derivative2
+{
+    my ($x, $y, $yp1, $ypn) = @_;
+    my $n = $#{$x};
+    my (@y2, @u);
+    my ($qn, $un);
+
+    croak "X and Y array lengths don't match." unless ($n == $#{$y});
+
+    if (defined $yp1)
+    {
+	$y2[0] = -0.5;
+	$u[0] = (3/($x->[1] - $x->[0])) * (($y->[1] - $y->[0])/($x->[1] - $x->[0]) - $yp1);
     }
-    else {
-	$y2[0]=-0.5;
-	$u[0]=(3/($x->[1]-$x->[0]))*(($y->[1]-$y->[0])/($x->[1]-$x->[0])-$yp1);
+    else
+    {
+	$y2[0] = 0;
+        $u[0] = 0;
     }
-    for($i=1; $i<$n; $i++) {
-	my $sig=($x->[$i]-$x->[$i-1])/($x->[$i+1]-$x->[$i-1]);
-	my $p=$sig*$y2[$i-1]+2.0; 	
-	$y2[$i]=($sig-1.0)/$p;
-	$u[$i]=(6.0*( ($y->[$i+1]-$y->[$i])/($x->[$i+1]-$x->[$i])-
-		      ($y->[$i]-$y->[$i-1])/($x->[$i]-$x->[$i-1])
+
+    for (my $i = 1; $i < $n; $i++)
+    {
+	my $sig = ($x->[$i] - $x->[$i-1])/($x->[$i+1] - $x->[$i-1]);
+	my $p = $sig * $y2[$i-1] + 2.0; 
+
+	$y2[$i] = ($sig - 1.0)/$p;
+	$u[$i] = (6.0 * ( ($y->[$i+1] - $y->[$i])/($x->[$i+1] - $x->[$i]) -
+		      ($y->[$i] - $y->[$i-1])/($x->[$i] - $x->[$i-1])
 		     )/
-		($x->[$i+1]-$x->[$i-1])-$sig*$u[$i-1])/$p;
+		($x->[$i+1] - $x->[$i-1]) - $sig * $u[$i-1])/$p;
     }
-    my ($qn,$un);
-    if (!defined $ypn) {
-	$qn=0;
-	$un=0;
+
+    if (defined $ypn)
+    {
+	$qn = 0.5;
+	$un = (3.0/($x->[$n]-$x->[$n-1])) *
+	    ($ypn - ($y->[$n] - $y->[$n-1])/($x->[$n] - $x->[$n-1]));
     }
-    else {
-	$qn=0.5;
-	$un=(3.0/($x->[$n]-$x->[$n-1]))*
-	    ($ypn-($y->[$n]-$y->[$n-1])/($x->[$n]-$x->[$n-1]));
+    else
+    {
+	$qn = 0;
+	$un = 0;
     }
-    $y2[$n]=($un-$qn*$u[$n-1])/($qn*$y2[$n-1]+1.0);
-    for($i=$n-1; $i>=0; --$i) {
-	$y2[$i]=$y2[$i]*$y2[$i+1]+$u[$i];
+
+    $y2[$n] = ($un - $qn * $u[$n-1])/($qn * $y2[$n-1] + 1.0);
+
+    for(my $i = $n-1; $i >= 0; --$i)
+    {
+	$y2[$i] = $y2[$i] * $y2[$i+1] + $u[$i];
     }
+
     return @y2;
 }
-
 
 1;
